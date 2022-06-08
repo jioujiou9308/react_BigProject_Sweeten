2c2e17c7717631e93bcffd63814df29c0b3ec56a
chl5566 <tukuchl5566@gmail.com>
2022-06-08 19:19:18 +0800

    新增 router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
-import Aside from "./layout/Aside";
+import Home from "./page/Home";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 import Main from "./layout/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="relative min-h-screen App bg-test text-dark">
-      <Header />
-      <main className="container flex flex-wrap justify-around py-12 mx-auto">
-        <Aside />
-        <div className="w-full md:w-3/4 ">
-          <Main />
-        </div>
-      </main>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main/*" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
