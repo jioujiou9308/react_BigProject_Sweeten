fbafa116df85ca638f6b0e070bf8c2a30b2a05dc
angelia-yingru <103923615+angelia-yingru@users.noreply.github.com>
2022-06-09 16:28:39 +0800

    add btns

--- src/page/Home.js
+++ src/page/Home.js
@@ -5,138 +5,197 @@ import cake1 from "../images/section01_cake1.png";
 const Home = () => {
   return (
     <>
-    <div class="w-full">
-      <img src={slider} alt="..." class="w-full"/>
-    </div>
-    <div class="flex justify-center bg-white">
-     <div class="p-5 ms-5 me-5 mt-5" style={{width: '75%'}}>
-      <div class="sec01_title">
-        <h1 class="text-center text-4xl font-bold">為了您，打造精彩的甜食饗宴</h1>
-        <p class="text-center p-5">
-          我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得
-        </p>
+      <div class="w-full">
+        <img src={slider} alt="..." class="w-full" />
       </div>
-      <div class="flex g-4 ms-5 me-5">
-        <div class="p-5">
-            <img
-              src={cake1}
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="text-center">我們不得不面</h5>
-              <p class="">
-                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
-              </p>
-            </div>
-        </div>
-        <div class="p-5">
-            <img
-              src={cake1}
-              alt="..."
-            />
-            <div class="card-body">
-              <h5 class="text-center">我們不得不面</h5>
-              <p class="">
-                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
-              </p>
-            </div>
-        </div>
-        <div class="p-5">
-            <img
-              src={cake1}
-              alt="..."
-            />
-            <div class="">
-              <h5 class="text-center">我們不得不面</h5>
-              <p class="">
-                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
-              </p>
-            </div>
-        </div>
-        <div class="p-5">
-            <img
-              src={cake1} alt="..."
-            />
-            <div class="">
-              <h5 class="text-center">我們不得不面</h5>
-              <p class="">
-                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
-              </p>
-            </div>
-        </div>
-      </div>
-    </div>
-    </div>
-
-    {/* section 02 */}
-    <section className="overflow-hidden relative bg-dark">
-          {/* desktop */}
-          <div className="hidden md:block">
-            <div className="max-w-full">
-                <img class="img-fluid position-absolute" src={require("../images/Shinnosuke11.png")} alt=""/>
-            </div>
-            <h1 class="position-absolute display-5">
-              <b>為了您，<br/>&nbsp;&nbsp;&nbsp;打造精彩的甜食饗宴</b>
+      <div class="flex justify-center bg-white">
+        <div class="p-5 ms-5 me-5 mt-5" style={{ width: "75%" }}>
+          <div class="sec01_title">
+            <h1 class="text-center text-4xl font-bold">
+              為了您，打造精彩的甜食饗宴
             </h1>
-            <div class="mask position-absolute text-center row justify-content-around">
-              <p class="align-self-end mb-4">我們不得不面對一個非常尷尬的事實，<br/>那就是，對於甜點，<br/>我們不能不去想，卻也不能走火入魔。<br/>我們要學會站在別人的角度思考。</p>
-              <div class="btn-wrap">
-                <a type="button" class="h5 to-peoduct-list-btn text-decoration-none text-white px-4 py-2" href="#/">前往商品列表</a>
+            <p class="text-center p-5">
+              我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得
+            </p>
+          </div>
+          <div class="flex g-4 ms-5 me-5">
+            <div class="p-5">
+              <img src={cake1} alt="..." />
+              <div class="my-2">
+                <h1 class="text-center text-lg font-normal my-5">
+                  活動限定/季節限定
+                </h1>
+                <p class="my-6">
+                  我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
+                </p>
+                <div class="flex justify-center">
+                <button class="btn-home">前往課程列表 ></button>
+                </div>
+              </div>
+            </div>
+            <div class="p-5">
+              <img src={cake1} alt="..." />
+              <div class="my-2 justify-center">
+                <h1 class="text-center text-lg font-normal my-5">經典熱銷</h1>
+                <p class="my-6">
+                  我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
+                </p>
+                <div class="flex justify-center">
+                <button class="btn-home">前往課程列表 ></button>
+                </div>
+              </div>
+            </div>
+            <div class="p-5">
+              <img src={cake1} alt="..." />
+              <div class="my-2">
+                <h1 class="text-center text-lg font-normal my-5">課程介紹</h1>
+                <p class="my-6">
+                  我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
+                </p>
+                <div class="flex justify-center">
+                <button class="btn-home">前往課程列表 ></button>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+      </div>
 
-          {/* mobile */}
-          <div class="mobile d-md-none">
-            <div class="homepage-section-2-imgclip mw-100">
-              <img class="img-fluid position-absolute" src="./images/image 3.png" alt=""/>
+      {/* section 02 */}
+      <section className="overflow-hidden relative bg-dark">
+        {/* desktop */}
+        <div className="hidden md:block">
+          <div className="max-w-full">
+            <img
+              class="img-fluid position-absolute"
+              src={require("../images/Shinnosuke11.png")}
+              alt=""
+            />
+          </div>
+          <h1 class="position-absolute display-5">
+            <b>
+              為了您，
+              <br />
+              &nbsp;&nbsp;&nbsp;打造精彩的甜食饗宴
+            </b>
+          </h1>
+          <div class="mask position-absolute text-center row justify-content-around">
+            <p class="align-self-end mb-4">
+              我們不得不面對一個非常尷尬的事實，
+              <br />
+              那就是，對於甜點，
+              <br />
+              我們不能不去想，卻也不能走火入魔。
+              <br />
+              我們要學會站在別人的角度思考。
+            </p>
+            <div class="btn-wrap">
+              <a
+                type="button"
+                class="h5 to-peoduct-list-btn text-decoration-none text-white px-4 py-2"
+                href="#/"
+              >
+                前往商品列表
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* mobile */}
+        <div class="mobile d-md-none">
+          <div class="homepage-section-2-imgclip mw-100">
+            <img
+              class="img-fluid position-absolute"
+              src="./images/image 3.png"
+              alt=""
+            />
           </div>
           <div class="mask position-absolute text-center row align-content-center">
             <h1 class="display-5">
-              <b>為了您，<br/>打造精彩的甜食饗宴</b>
+              <b>
+                為了您，
+                <br />
+                打造精彩的甜食饗宴
+              </b>
             </h1>
-            <p class="mt-4">我們不得不面對一個非常尷尬的事實，<br/>那就是，對於甜點，<br/>我們不能不去想，卻也不能走火入魔。<br/>我們要學會站在別人的角度思考。</p>
+            <p class="mt-4">
+              我們不得不面對一個非常尷尬的事實，
+              <br />
+              那就是，對於甜點，
+              <br />
+              我們不能不去想，卻也不能走火入魔。
+              <br />
+              我們要學會站在別人的角度思考。
+            </p>
             <div class="btn-wrap">
-              <a type="button" class="h5 to-peoduct-list-btn text-decoration-none text-white px-4 py-2" href="#/">前往商品列表</a>
+              <a
+                type="button"
+                class="h5 to-peoduct-list-btn text-decoration-none text-white px-4 py-2"
+                href="#/"
+              >
+                前往商品列表
+              </a>
             </div>
           </div>
-          </div>
-        </section>
+        </div>
+      </section>
 
       {/* section 03 */}
       <section class="homepage-section-3 container px-0">
         <div class="homepage-card-wrap row justify-content-around gy-5">
-
           <div class="card p-0 col-md-3 mx-3">
-            <img src="./images/Rectangle 5.png" class="card-img-top" alt="..."/>
+            <img
+              src="./images/Rectangle 5.png"
+              class="card-img-top"
+              alt="..."
+            />
             <div class="card-body text-center">
               <h5 class="card-title">我們不得不面對一個</h5>
-              <p class="card-text">我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實</p>
-              <a href="#/" class="btn">我們不得不</a>
+              <p class="card-text">
+                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
+              </p>
+              <a href="#/" class="btn">
+                我們不得不
+              </a>
             </div>
           </div>
 
           <div class="card p-0 col-md-3 mx-3">
-            <img src="./images/Rectangle 6.png" class="card-img-top" alt="..."/>
+            <img
+              src="./images/Rectangle 6.png"
+              class="card-img-top"
+              alt="..."
+            />
             <div class="card-body text-center">
               <h5 class="card-title">我們不得不面對一個</h5>
-              <p class="card-text">我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實</p>
-              <a href="#/" class="btn">我們不得不</a>
+              <p class="card-text">
+                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
+              </p>
+              <a href="#/" class="btn">
+                我們不得不
+              </a>
             </div>
           </div>
 
           <div class="card p-0 col-md-3 mx-3">
-            <img src="./images/Rectangle 7.png" class="card-img-top" alt="..."/>
+            <img
+              src="./images/Rectangle 7.png"
+              class="card-img-top"
+              alt="..."
+            />
             <div class="card-body text-center">
               <h5 class="card-title">我們不得不面對一個</h5>
-              <p class="card-text">我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實</p>
-              <a href="#/" class="btn">我們不得不</a>
+              <p class="card-text">
+                我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實我們不得不面對一個非常尷尬的事實
+              </p>
+              <a href="#/" class="btn">
+                我們不得不
+              </a>
             </div>
           </div>
-
         </div>
       </section>
     </>
   );
 };
-export default Home
+export default Home;
