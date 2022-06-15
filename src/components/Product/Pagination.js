b8a13c0751391765acc25d0c48538708c771d4bd
angelia-yingru <103923615+angelia-yingru@users.noreply.github.com>
2022-06-15 10:49:28 +0800

    layout of credit card details

--- src/components/Product/Pagination.js
+++ src/components/Product/Pagination.js
@@ -9,22 +9,22 @@ const Pagination = () => {
             <span className="mx-1">previous</span>
           </a>
         </li>
-        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover:text-gray-200">
+        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover: ">
           <a className="font-bold" href="#">
             1
           </a>
         </li>
-        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover:text-gray-200">
+        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover: ">
           <a className="font-bold" href="#">
             2
           </a>
         </li>
-        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover:text-gray-200">
+        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover: ">
           <a className="font-bold" href="#">
             3
           </a>
         </li>
-        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover:text-gray-200">
+        <li className="px-3 py-2 mx-1 text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-700 hover: ">
           <a className="flex items-center font-bold" href="#">
             <span className="mx-1">Next</span>
           </a>
