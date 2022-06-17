f36fa2c44dd0b16964072dce420808088f1f0db1
Mandy <qqamandy@gmail.com>
2022-06-17 12:57:12 +0800

    memberOrder可以撈到假資料惹

--- src/page/MemberOrder.js
+++ src/page/MemberOrder.js
@@ -3,6 +3,27 @@ import Member_collection_bar from "../components/memberCollection/Member_collect
 import Member_search_bar from "../components/memberCollection/Member_search_bar";
 
 function MemberOrder() {
+  // 假資料
+  const products = [
+    {
+      id: 1,
+      name: "標哥千層蛋糕",
+      count: 4,
+      price: 500,
+    },
+    {
+      id: 2,
+      name: "蜂蜜蛋糕",
+      count: 5,
+      price: 400,
+    },
+    {
+      id: 3,
+      name: "大麻蛋糕",
+      count: 2,
+      price: 750,
+    },
+  ];
   return (
     <>
       <div className="bg-white">
@@ -15,53 +36,35 @@ function MemberOrder() {
         <div className="pt-2 mx-5 text-right border-b ">
           <h2>訂單 # 123456</h2>
         </div>
-        {/* 兩兩一組ㄉdiv */}
-        <div className="md:flex">
-          {/* DEMO1 */}
-          <div className="flex items-center justify-start p-5 border-b md:w-1/2">
-            <div className="mr-2 overflow-hidden">
-              <img
-                className="scale-105 "
-                src={`${process.env.PUBLIC_URL}/images/memberCollectionAndOrder/member_order.png`}
-                alt=""
-              />
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          {products.map((product, i) => {
+            const { id, name, count, price } = product;
+            return (
+              <>
+                <div className="flex items-center justify-start p-5 border-b ">
+                  <div className="mr-2 overflow-hidden">
+                    <img
+                      className="scale-105 "
+                      src={`${process.env.PUBLIC_URL}/images/memberCollectionAndOrder/member_order.png`}
+                      alt=""
+                    />
+                  </div>
 
-            <div className="mx-3">
-              <p>商品</p>
-              <p className="my-2">數量</p>
-              <p>價格</p>
-            </div>
+                  <div className="mx-3">
+                    <p>商品</p>
+                    <p className="my-2">數量</p>
+                    <p>價格</p>
+                  </div>
 
-            <div>
-              <p>標哥千層蛋糕</p>
-              <p className="my-2">* 1</p>
-              <p>800</p>
-            </div>
-          </div>
-
-          {/* DEMO2 */}
-          <div className="flex items-center justify-start p-5 border-b md:w-1/2">
-            <div className="mr-2 overflow-hidden">
-              <img
-                className="scale-105 "
-                src={`${process.env.PUBLIC_URL}/images/memberCollectionAndOrder/member_order.png`}
-                alt=""
-              />
-            </div>
-
-            <div className="mx-3">
-              <p>商品</p>
-              <p className="my-2">數量</p>
-              <p>價格</p>
-            </div>
-
-            <div>
-              <p>標哥千層蛋糕</p>
-              <p className="my-2">* 1</p>
-              <p>800</p>
-            </div>
-          </div>
+                  <div>
+                    <p>{name}</p>
+                    <p className="my-2">* {count}</p>
+                    <p>{price}</p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
         </div>
       </div>
     </>
