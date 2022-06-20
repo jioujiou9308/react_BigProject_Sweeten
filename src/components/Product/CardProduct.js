c30196b51d4800010ff9fb8f80b2697bd2996e52
= <=>
2022-06-20 13:57:47 +0800

    product更新

--- src/components/Product/CardProduct.js
+++ src/components/Product/CardProduct.js
@@ -6,17 +6,17 @@ const CardProduct = () => {
     <>
       <section className="bg-white dark:bg-gray-900">
         <div className="container px-6 py-8 mx-auto">
-          {/* <div className="flex items-center justify-between text-sm tracking-widest uppercase ">
-                <p className="text-gray-500 dark:text-gray-300">6 Items</p>
-                <div className="flex items-center">
-                  <p className="text-gray-500 dark:text-gray-300">Sort</p>
-                  <select className="font-medium text-gray-700 bg-transparent dark:text-gray-500 focus:outline-none">
-                    <option value="#">Recommended</option>
-                    <option value="#">Size</option>
-                    <option value="#">Price</option>
-                  </select>
-                </div>
-              </div> */}
+          <div className="flex items-center justify-between text-sm tracking-widest uppercase ">
+            <p className="text-gray-500 dark:text-gray-300">6 Items</p>
+            <div className="flex items-center">
+              <p className="text-gray-500 dark:text-gray-300">Sort</p>
+              <select className="font-medium text-gray-700 bg-transparent dark:text-gray-500 focus:outline-none">
+                <option value="#">Recommended</option>
+                <option value="#">Size</option>
+                <option value="#">Price</option>
+              </select>
+            </div>
+          </div>
           {/* ---------------------------- */}
           <div className="flex flex-wrap">
             <OneCardProduct />
