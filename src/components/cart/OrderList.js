f4ba3f406a44033546e127441c40abc963df8d15
Holin5566 <96641400+Holin5566@users.noreply.github.com>
2022-06-28 10:12:12 +0800

    fix filename

--- src/components/cart/OrderList.js
+++ src/components/cart/OrderList.js
@@ -9,19 +9,19 @@ function OrderList(props) {
       <div className="p-8 bg-white lg:w-2/3 md:p-12" id="scroll">
         <p className="font-black h2">Bag</p>
         {productsInOrder.map((product, i) => {
-          const { id, name, categroy, image, price, count, size, flavor } = product;
+          const { id, name, categroy, image, price, count, size, flavor } =
+            product;
           return (
             <ProductItem
               key={id}
-              
-               id={id}
-               name={name}
-               categroy={categroy}
-               price={price}
-               image={image}
-               count={count}
-               size={size}
-               flavor={flavor}
+              id={id}
+              name={name}
+              categroy={categroy}
+              price={price}
+              image={image}
+              count={count}
+              size={size}
+              flavor={flavor}
               setCount={(newCount) => {
                 const newProductsInOrder = productsInOrder.map((value) => {
                   return { ...value };
