d4f3207736975ff3a37c20bbc60c3f38da94935f
angelia-yingru <103923615+angelia-yingru@users.noreply.github.com>
2022-06-29 15:39:45 +0800

    merge

---
+++ src/components/product/CardProduct.js
@@ -0,0 +1,69 @@
+import React from "react";
+import OneCardProduct from "./OneCardProduct";
+import { useProductState } from "../../utils/redux/hooks-redux";
+
+
+const products = [
+  {
+    id: 1,
+    name: "馬卡龍蛋糕",
+    price: 400,
+    img: "/images/course/hand.jpg",
+  },
+  {
+    id: 2,
+    name: "龍蛋糕",
+    price: 450,
+    img: "/images/course/hand.jpg",
+  },
+  {
+    id: 3,
+    name: "卡龍蛋糕",
+    price: 780,
+    img: "/images/course/hand.jpg",
+  },
+  {
+    id: 4,
+    name: "馬龍蛋糕",
+    price: 300,
+    img: "/images/course/hand.jpg",
+  },
+];
+
+const CardProduct = () => {
+  const [product, setProduct] = useProductState();
+ 
+  return (
+    <>
+      <section className="bg-white">
+        <div className="flex items-center justify-between text-sm tracking-widest uppercase md:px-3">
+          <h3 className="w-full p-1 border-b-2 border-line p">精選商品</h3>
+          {/* <p className="text-gray-500 dark:text-gray-300">
+            {products.length} Items
+          </p>
+          <div className="flex items-center">
+            <p className="text-gray-500 dark:text-gray-300">Sort</p>
+            <select className="font-medium text-gray-700 bg-transparent dark:text-gray-500 focus:outline-none">
+              <option value="#">Recommended</option>
+              <option value="#">Size</option>
+              <option value="#">Price</option>
+            </select>
+          </div> */}
+        </div>
+        <div className="container px-6 py-8 mx-auto">
+          {/* ---------------------------- */}
+          <div className="flex flex-wrap justify-around">
+            {products.map((product, i) => {
+              const { id, name, price, img } = product;
+              return (
+                <OneCardProduct id={id} name={name} price={price} img={img} />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CardProduct;
---
+++ src/components/product/OneCardProduct.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { AiOutlineMessage, AiOutlineHeart } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { useProductState } from "../../utils/redux/hooks-redux";
+
+const OnceCarkProduct = (props) => {
+  const { id, name, price, img } = props;
+  const [product, serProduct] = useProductState();
+  const navigate = useNavigate()
+  return (
+    <>
+      <div className=" mb-6 mx-1 xl:w-[13rem]">
+        <div className="flex flex-col items-center justify-center mr-1 ">
+          <img
+            className="object-cover w-full rounded-sm h-[15rem]"
+            src={img}
+            alt="T-Shirt"
+            onClick={()=>{
+              navigate('/main/product/detail')
+            }}
+          />
+          <div className="flex items-center justify-end w-full mt-2">
+            <h4 className="w-full text-lg font-medium dark:text-gray-200">
+              {name}
+            </h4>
+            <div className="mr-2 text-blue-500 p">${price}</div>
+            <div className="flex items-center ">
+              <AiOutlineMessage className="icon-sm" />
+              {/* <AiOutlineShoppingCart className="icon" /> */}
+              <AiOutlineHeart className="icon-sm" />
+            </div>
+          </div>
+
+          <button
+            className="flex items-center justify-center w-full px-2 py-2 mt-4 text-white rounded-sm opacity-100 hover:opacity-80 bg-dark focus:outline-none "
+            onClick={() => {
+              serProduct([...product, "click"]);
+            }}
+          >
+            <span className="mx-1">加入購物車</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OnceCarkProduct;
